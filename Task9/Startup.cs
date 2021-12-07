2a102cdbd9a91af98f90f7e115a4f4d1362a6f01
weavelol <deniss.shutov98@gmail.com>
2021-12-07 02:53:32 +0200

    2

--- Task9/Startup.cs
+++ Task9/Startup.cs
@@ -38,16 +38,6 @@ namespace Task9 {
                 context.Database.Migrate();
             }
 
-            if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
-            }
-            else {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints => {
