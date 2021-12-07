7fac1f051e66d8e9c06a17595b1bbe251d62c40b
weavelol <deniss.shutov98@gmail.com>
2021-12-07 02:51:25 +0200

    ?

--- Data/AbstractRepository.cs
+++ Data/AbstractRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Interfaces;
-using Microsoft.EntityFrameworkCore;
 
 namespace Data {
     public abstract class AbstractRepository<T> :IRepository<T> {
