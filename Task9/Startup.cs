97813b98b828bb2fb65d852cf21b3fae65b0f2e9
weavelol <deniss.shutov98@gmail.com>
2021-12-10 15:14:37 +0200

    1

--- Task9/Startup.cs
+++ Task9/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using AutoMapper;
-using Core.Models;
 using Data;
 using Data.Repositories;
 using Microsoft.AspNetCore.Builder;
@@ -8,7 +7,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Hosting;
 using Services.ModelsDTO;
 using Services.Services;
 using ServicesInterfaces;
